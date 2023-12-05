a13dbe5570cf9c031d8495f9864d299d4a3ace72
noushad257 <120626388+noushad257@users.noreply.github.com>
2023-12-06 01:45:52 +0600

    operators

---
+++ operators.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+ using namespace std;
+ int main()
+ {
+     int a=2.0/5;
+     cout<<a<<endl;
+     cout<<2.0/5<<endl;
+
+     //relational operator
+     //=,>,<,>=,<=,!=
+
+     int b=2;
+     int c=10;
+
+     bool first=(b==c);
+     cout<<first<<endl;
+
+
+     bool secondly=(b>=c);
+     cout<<secondly<<endl;
+
+
+     bool third=(b<=c);
+     cout<<third<<endl;
+
+
+     bool fourth=(b>c);
+     cout<<fourth<<endl;
+
+
+     bool fifth=(b!=c);
+     cout<<fifth<<endl;
+
+
+
+
+
+
+
+
+
+ }
