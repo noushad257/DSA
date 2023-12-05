419c20bdce5450310e310bc64c33bfe245096b9b
noushad257 <120626388+noushad257@users.noreply.github.com>
2023-12-06 01:45:24 +0600

    first c program include data type

---
+++ 1.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int main()
+{   //data type
+    /*
+    int a=123;
+    cout<<a<<endl;
+
+    char b='v';
+    cout<<b<<endl;
+
+    bool b1=true;
+    cout<<b1<<endl;
+
+    float f=1.2;
+    cout<<f<<endl;
+
+    double d=1.23;
+    cout<<d<<endl;
+
+    int size =sizeof(a);
+    cout<<"size of a is "<<size<<endl;
+
+    //first c program
+   cout<<"hello world"<<endl;*/
+
+
+   //type casting
+   /*int a='a';
+   cout<<a<<endl;
+
+   char ch=98;
+   cout<<ch<<endl;
+   */
+
+   //signed unsigned
+
+   unsigned int a=-123;
+   cout<<a<<endl;
+
+
+}
