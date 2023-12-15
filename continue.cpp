34c866447249639a9208a92add65d90830e2cd64
noushad <120626388+noushad257@users.noreply.github.com>
2023-12-15 22:24:17 +0600

    Add files via upload

---
+++ continue.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=0;i<=1;i++)
+    {
+        cout<<"yo"<<endl;
+
+        cout<<"hello ji"<<endl;
+
+        cout<<"Bhaiya ji mujhe maf kardo"<<endl;
+        continue;
+
+        cout<<"Main ithna bhi khas nehi"<<endl;
+        cout<<" :'( "<<endl;
+
+
+    }
+
+
+
+
+}
